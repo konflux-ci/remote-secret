cdbaa95b5913f6cbfaec814f23d311d7b37718f2
Max Shaposhnyk <mshaposh@redhat.com>
2023-08-16 21:15:02 +0300

    Add openshift config
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>

--- hack/generate_webhook_ca.sh
+++ hack/generate_webhook_ca.sh
@@ -13,7 +13,7 @@ GENCERTS_DIR="${TEMP_DIR}/webhook/k8s/certs"
 mkdir -p "${GENCERTS_DIR}"
 
 openssl genrsa -out ${GENCERTS_DIR}/ca.key 2048
-openssl req -x509 -new -nodes -key ${GENCERTS_DIR}/ca.key -subj "/CN=remote-secret-webhook-service.remotesecret.svc" -days 10000 -out ${GENCERTS_DIR}/ca.crt
+openssl req -x509 -new -nodes -key ${GENCERTS_DIR}/ca.key -subj "/CN=webhook-service.remotesecret.svc" -days 10000 -out ${GENCERTS_DIR}/ca.crt
 openssl genrsa -out ${GENCERTS_DIR}/tls.key 2048
 openssl req -new -key ${GENCERTS_DIR}/tls.key -out ${GENCERTS_DIR}/tls.csr -config ${THIS_DIR}/csr.conf
 openssl x509 -req -in ${GENCERTS_DIR}/tls.csr -CA ${GENCERTS_DIR}/ca.crt -CAkey ${GENCERTS_DIR}/ca.key -CAcreateserial -out ${GENCERTS_DIR}/tls.crt -days 10000 -extensions v3_ext -extfile ${THIS_DIR}/csr.conf -sha256
