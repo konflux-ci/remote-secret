55d4c0680d0fe4e2d68d2f1f84042492cef7e3f3
Max Shaposhnyk <mshaposh@redhat.com>
2023-08-24 10:40:17 +0300

    Docs fixup
    Signed-off-by: Max Shaposhnyk <mshaposh@redhat.com>

--- api/v1beta1/remotesecret_types.go
+++ api/v1beta1/remotesecret_types.go
@@ -109,10 +109,9 @@ type RemoteSecret struct {
 
 	Spec   RemoteSecretSpec   `json:"spec,omitempty"`
 	Status RemoteSecretStatus `json:"status,omitempty"`
-	// Temporary optional field for data to upload during create/update RemoteSecret
+	// Transient optional field for data to upload during create/update RemoteSecret
 	// It is processed by Mutating Webhook and must not be persisted,
 	// to make sure (in a case if something happened with Webhook) it is constrained
-	// by MaxItems=0, made as array of objects since it looks like it does not work for map
 	//+kubebuilder:validation:MaxProperties=0
 	UploadData map[string]string `json:"data,omitempty"`
 }
