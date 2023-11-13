a8023ac350fc16876503ee35d5b418f03a9178b6
Sergii Kabashniuk <skabashniuk@redhat.com>
2023-11-13 09:12:37 +0200

    fixup! Fix formatter
    Signed-off-by: Sergii Kabashniuk <skabashniuk@redhat.com>

--- pkg/secretstorage/es/vault.go
+++ pkg/secretstorage/es/vault.go
@@ -17,5 +17,5 @@ package es
 import vault "github.com/external-secrets/external-secrets/pkg/provider/vault"
 
 type VaultSecretStorage struct {
-	Conntector vault.Connector
+	Connector vault.Connector
 }
