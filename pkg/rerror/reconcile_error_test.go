e2cb292dec156beb5d2a2d0de37c0fae4b31d37e
Rafaela Soares <rsoaresd@redhat.com>
2023-12-14 10:42:25 +0000

    fix

--- pkg/rerror/reconcile_error_test.go
+++ pkg/rerror/reconcile_error_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//nolint:goerr113 the errors are 'fake'
+//nolint:goerr113 // the errors are 'fake'
 package rerror
 
 import (
