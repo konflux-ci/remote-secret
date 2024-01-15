6054f3703a7a9a8de6093f4166872660ee29e087
Sergii Kabashniuk <skabashniuk@redhat.com>
2024-01-15 14:29:49 +0200

    More checks

--- integration_tests/remotesecretcontroller_test.go
+++ integration_tests/remotesecretcontroller_test.go
@@ -1021,23 +1021,29 @@ var _ = Describe("RemoteSecret", func() {
 					g.Expect(rs.Status.Targets).To(HaveLen(1))
 					g.Expect(rs.Status.Targets[0].SecretName).To(Equal("exact-secret-name")) //nolint:staticcheck // SA1019 - this deprecated field needs to be set
 					g.Expect(rs.Status.Targets[0].ExpectedSecret).To(BeNil())
-					//ExpectStatusConditionMetric(ITest.Registry, []*StatusConditionValue{
-					//	{
-					//		Condition: "DataObtained",
-					//		Name:      "test-remote-secret",
-					//		Namespace: "default",
-					//		Status:    "True",
-					//		Value:     1,
-					//	},
-					//	{
-					//		Condition: "Deployed",
-					//		Name:      "test-remote-secret",
-					//		Namespace: "default",
-					//		Status:    "True",
-					//		Value:     1,
-					//	},
-					//})
-
+					ExpectStatusConditionMetric(ITest.Registry, []*StatusConditionValue{
+						{
+							Condition: "DataObtained",
+							Name:      "test-remote-secret",
+							Namespace: "default",
+							Status:    "False",
+							Value:     0,
+						},
+						{
+							Condition: "DataObtained",
+							Name:      "test-remote-secret",
+							Namespace: "default",
+							Status:    "True",
+							Value:     1,
+						},
+						{
+							Condition: "Deployed",
+							Name:      "test-remote-secret",
+							Namespace: "default",
+							Status:    "True",
+							Value:     1,
+						},
+					})
 				})
 			})
 		})
