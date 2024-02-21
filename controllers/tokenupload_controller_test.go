e7da3a83d74515688dd24ff952fe1b1cd8784d65
Martin Mulholland <mmulholl@redhat.com>
2024-02-21 08:18:57 -0500

    working test

--- controllers/tokenupload_controller_test.go
+++ controllers/tokenupload_controller_test.go
@@ -4,20 +4,48 @@ import (
 	"context"
 	"testing"
 
+	api "github.com/redhat-appstudio/remote-secret/api/v1beta1"
+
 	"github.com/stretchr/testify/assert"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	corev1 "k8s.io/api/core/v1"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
 func TestCreateRemoteSecret(t *testing.T) {
-	
+
+	remoteSecret := &api.RemoteSecret{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "appstudio.redhat.com/v1beta1",
+			Kind:       "RemoteSecret",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "test-remote-secret",
+		},
+	}
+
+	rs_gvc := schema.GroupVersionKind{
+		Group:   "appstudio.redhat.com",
+		Version: "v1beta",
+		Kind:    "RemoteSecret",
+	}
+
+	scheme := runtime.NewScheme()
+	scheme.AddKnownTypeWithName(rs_gvc, &api.RemoteSecret{})
+	assert.NoError(t, corev1.AddToScheme(scheme))
+	cl := fake.NewClientBuilder().
+		WithScheme(scheme).
+		WithObjects(remoteSecret).Build()
+
 	TUr := TokenUploadReconciler{
-		Client:              fake.NewClientBuilder().Build(),
+		Client:              cl,
 		Scheme:              nil,
-		RemoteSecretStorage: rss,
+		RemoteSecretStorage: nil,
 	}
 
 	// Define the secret here to avoid repetition and only overwrite the specific parts in each test case.
